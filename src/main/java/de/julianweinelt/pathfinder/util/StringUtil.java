542ce4d471fcb5127c55a8d5614acc0be3c51985
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-22 11:55:44 +0200

    api: remove @NotNull annotations from StringUtil

--- src/main/java/de/julianweinelt/pathfinder/util/StringUtil.java
+++ src/main/java/de/julianweinelt/pathfinder/util/StringUtil.java
@@ -1,12 +1,11 @@
 package de.julianweinelt.pathfinder.util;
 
 import com.google.common.base.Preconditions;
-import com.sun.istack.internal.NotNull;
 
 import java.util.Collection;
 
 public class StringUtil {
-    public static <T extends Collection<? super String>> @NotNull T copyPartialMatches(@NotNull String token, @NotNull Iterable<String> originals, @NotNull T collection) throws UnsupportedOperationException, IllegalArgumentException {
+    public static <T extends Collection<? super String>> T copyPartialMatches(String token, Iterable<String> originals, T collection) throws UnsupportedOperationException, IllegalArgumentException {
         Preconditions.checkArgument(token != null, "Search token cannot be null");
         Preconditions.checkArgument(collection != null, "Collection cannot be null");
         Preconditions.checkArgument(originals != null, "Originals cannot be null");
@@ -20,7 +19,7 @@ public class StringUtil {
         return collection;
     }
 
-    public static boolean startsWithIgnoreCase(@NotNull String string, @NotNull String prefix) throws IllegalArgumentException, NullPointerException {
+    public static boolean startsWithIgnoreCase(String string, String prefix) throws IllegalArgumentException, NullPointerException {
         Preconditions.checkArgument(string != null, "Cannot check a null string for a match");
         return string.length() < prefix.length() ? false : string.regionMatches(true, 0, prefix, 0, prefix.length());
     }
