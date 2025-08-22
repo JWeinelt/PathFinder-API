3f8dc7ec2b886d7141ff9efc3653c0c4925a4f2e
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-22 11:50:42 +0200

    api: improve CustomListProvider.java

--- src/main/java/de/julianweinelt/pathfinder/suggestion/CustomListProvider.java
+++ src/main/java/de/julianweinelt/pathfinder/suggestion/CustomListProvider.java
@@ -1,11 +1,7 @@
 package de.julianweinelt.pathfinder.suggestion;
 
+import java.util.List;
+
 public interface CustomListProvider {
-    /**
-     * Provides a list of suggestions based on the given input.
-     *
-     * @param input The input string to base suggestions on.
-     * @return An array of suggestions.
-     */
-    String[] provideSuggestions(String input);
+    List<String> onSuggest();
 }
