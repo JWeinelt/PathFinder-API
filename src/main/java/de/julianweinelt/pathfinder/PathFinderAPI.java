80e86efdcbcaad0441daf740dfcb06da359bd8f1
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-24 14:45:52 +0200

    api: change version string

--- src/main/java/de/julianweinelt/pathfinder/PathFinderAPI.java
+++ src/main/java/de/julianweinelt/pathfinder/PathFinderAPI.java
@@ -15,7 +15,7 @@ public class PathFinderAPI
 {
     public static final String MODID = "pathfinder";
     public static final String NAME = "PathFinder API";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static Logger logger;
 
