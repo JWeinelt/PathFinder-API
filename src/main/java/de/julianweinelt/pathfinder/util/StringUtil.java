e5f787e61a2a4e142d400f7fa7160560ce593740
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-16 14:45:58 +0200

    fix: remove Preconditions from StringUtil

--- src/main/java/de/julianweinelt/pathfinder/util/StringUtil.java
+++ src/main/java/de/julianweinelt/pathfinder/util/StringUtil.java
@@ -1,15 +1,11 @@
 package de.julianweinelt.pathfinder.util;
 
-import com.google.common.base.Preconditions;
 
 import java.util.Collection;
 
 public class StringUtil {
-    public static <T extends Collection<? super String>> T copyPartialMatches(String token, Iterable<String> originals, T collection) throws UnsupportedOperationException, IllegalArgumentException {
-        Preconditions.checkArgument(token != null, "Search token cannot be null");
-        Preconditions.checkArgument(collection != null, "Collection cannot be null");
-        Preconditions.checkArgument(originals != null, "Originals cannot be null");
-
+    public static <T extends Collection<? super String>> T copyPartialMatches(String token, Iterable<String> originals, T collection)
+            throws UnsupportedOperationException, IllegalArgumentException {
         for(String string : originals) {
             if (startsWithIgnoreCase(string, token)) {
                 collection.add(string);
@@ -20,7 +16,6 @@ public class StringUtil {
     }
 
     public static boolean startsWithIgnoreCase(String string, String prefix) throws IllegalArgumentException, NullPointerException {
-        Preconditions.checkArgument(string != null, "Cannot check a null string for a match");
         return string.length() >= prefix.length() && string.regionMatches(true, 0, prefix, 0, prefix.length());
     }
 }
